fb9299d2cf02d59b944a26f1df5aef0f999a2659
Yajas Sardana <yajassardana@gmail.com>
2020-08-26 01:24:03 +0530

    variables

---
+++ hello.ts
@@ -0,0 +1,5 @@
+ function log(message){
+   console.log(message);
+ }
+ let message = 'Hello world';
+ log(message);
---
+++ variable.ts
@@ -0,0 +1,10 @@
+let a; //any by default
+let b:number;
+let c:boolean;
+let d:string;
+let e:any;//like js let, type not known yet
+let f: number[]; //array
+let g:Object;// object
+
+enum color{Red=0,Green=1,Blue=2}; //enums in JS
+let backgroundColor = color.Red; 
