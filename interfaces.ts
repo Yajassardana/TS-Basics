864392e1c42000db41bc68d51fa71146fceea4bd
Yajas Sardana <yajassardana@gmail.com>
2020-08-29 01:55:59 +0530

    interfaces

---
+++ interfaces.ts
@@ -0,0 +1,17 @@
+interface Point{
+  x:number,
+  y:number
+}
+
+let drawPoint = (point:Point)=>{
+//
+}
+
+drawPoint({x:1,y:1});
+
+
+let drawPointTwo = (x:number,y:number)=>{
+//
+}
+
+drawPointTwo(1,1);
